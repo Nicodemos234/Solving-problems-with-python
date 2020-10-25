b9098981c4d8a36ecded65f398871ff0ebc72af5
Anny K Fernandes <32650449+Annykfs@users.noreply.github.com>
2020-10-25 14:11:54 -0300

    Update questao07.py

---
+++ questao07.py
@@ -0,0 +1,13 @@
+
+x = 0
+i = 0
+
+while x < 1 or x > 10:
+    x = int(input("insira um valor inteiro de 1 a 10: "))
+
+while i <= 10:
+    z = i*x
+    print(str(x)+"x"+str(i)+" = "+str(z))
+    i = i+1
+
+
